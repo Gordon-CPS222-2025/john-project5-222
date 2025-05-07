6cebb54685735e76be704e27697046a0782d7991
john mungai <john.mungai@gordon.edu>
2025-05-07 18:43:57 -0400

    added my road class and some comments to town and road, about to do the high way network class

---
+++ Road.cc
@@ -0,0 +1,24 @@
+#include "Road.h"
+
+
+// Constructor: Initializes a Road with destination town, distance, and bridge status
+// @param destination - Pointer to the Town this road leads to
+// @param distance   - Length/weight of the road (in arbitrary units)
+// @param isBridge   - Boolean flag indicating if this road is a bridge
+Road::Road(Town* destination, float distance, bool isBridge)
+    : destination(destination), distance(distance), isBridge(isBridge) {}
+
+// Getter method for the destination town
+Town* Road::getDestination() const {
+    return destination;
+}
+
+// Getter method for the road's distance/length
+float Road::getDistance() const {
+    return distance;
+}
+
+// Getter method for bridge status
+bool Road::getIsBridge() const {
+    return isBridge;
+}
--- Town.cc
+++ Town.cc
@@ -1,16 +1,22 @@
 #include "Town.h"
 #include "Road.h"
 
+
+// Constructor: Initializes a Town with the given name
+// @param name - The name of the town to be created
 Town::Town(const std::string& name) : name(name) {}
 
+// Getter method for the town's name
 const std::string& Town::getName() const {
     return name;
 }
 
+// Adds a road connection to this town
 void Town::addRoad(Road* road) {
     roads.push_back(road);
 }
 
+// Getter method for all roads connected to this town
 const std::vector<Road*>& Town::getRoads() const {
     return roads;
 }
